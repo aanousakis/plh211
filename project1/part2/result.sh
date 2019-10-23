766869f7b3822a5a48462ec3cc1e303e5167198a
aanousakis <anousakis.antonis@gmail.com>
2019-10-23 18:53:24 +0300

    support space in team name

--- project1/part2/result.sh
+++ project1/part2/result.sh
@@ -6,7 +6,8 @@
 
    awk -F"-|:"  '$3>$4 { score1=3; score2=0 } $3<$4 {score1=0; score2=3} $3==$4 {score1=1; score2=1} {print $1 "\t" score1 "\t" $3 "\t" $4 "\n" $2 "\t" score2 "\t" $4 "\t" $3}' $1 \
 |  sort \
-| awk '{
+| awk -F"\t"   \
+  '{
     arr1[$1]+=$2;
     arr2[$1]+=$3;
     arr3[$1]+=$4
@@ -14,7 +15,7 @@
 
     if (length($1) > width)
         width = length($1)
-
+ 
    }
    END {
      for (key in arr1) printf("\t%-*s\t%3s\t%3s-%-3s\n",width, key, arr1[key], arr2[key], arr3[key])
