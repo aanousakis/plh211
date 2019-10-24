d1681437ac6cb6fa2c6f4e7372b0d47579ef4af1
aanousakis <anousakis.antonis@gmail.com>
2019-10-24 11:15:33 +0300

    final

--- project1/part2/result.sh
+++ project1/part2/result.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-
-
-
-
-   awk -F"-|:"  '$3>$4 { score1=3; score2=0 } $3<$4 {score1=0; score2=3} $3==$4 {score1=1; score2=1} {print $1 "\t" score1 "\t" $3 "\t" $4 "\n" $2 "\t" score2 "\t" $4 "\t" $3}' $1 \
+   
+   #h eisodos xorizetai se stiles me tous diaxoristes "-" h ":"
+   awk -F"-|:"  \
+   '
+      $3>$4 { score1=3; score2=0} 
+      $3<$4 { score1=0; score2=3} 
+      $3==$4 {score1=1; score2=1} 
+      {print $1 "\t" score1 "\t" $3 "\t" $4 "\n" $2 "\t" score2 "\t" $4 "\t" $3}' $1 `#gia kathe agona upologizontai oi bathmoi kathe omadas`\
 |  sort \
 | awk -F"\t"   \
   '{
@@ -18,8 +20,8 @@
  
    }
    END {
-     for (key in arr1) printf("\t%-*s\t%3s\t%3s-%-3s\n",width, key, arr1[key], arr2[key], arr3[key])
+     for (key in arr1) printf("\t%-*s\t%3s\t%3s -%3s\n",width, key, arr1[key], arr2[key], arr3[key])
    }'\
 |  sort -k2,2nr -k1,1 \
-|  nl -w2 -s "."
+|  nl -n ln -w1 -s "."
 
